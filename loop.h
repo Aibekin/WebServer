1fac6104d9e5ce761f6774de2f2e0d2ec49bb096
Aibekin <aibekin_zh_007@mail.ru>
2024-09-10 00:08:10 +0500

    adding a time identifier

--- loop.h
+++ loop.h
@@ -1,20 +1,38 @@
-void loop_func(int a, int b, bool isWork, int led, int work, int rest) {
-  if (isWork) {
-    a -= 1;
-    digitalWrite(led, HIGH);
-    Serial.println(a);
-    if (a <= 0) {
-      isWork = false;
-      a = work;
-    }
+#ifndef LOOP_H
+#define LOOP_H
+
+#include <ESP8266WebServer.h>
+
+extern ESP8266WebServer server;
+
+void loop_func(int &a, int &b, bool &isWork, int led, int air, int fan, int work, int rest, bool isAuto) {
+  if (isAuto) {
+    Serial.println("automode");
   } else {
-    b -= 1;
-    digitalWrite(led, LOW);
-    Serial.println(b);
-    if (b <= 0) {
-      isWork = true;
-      b = rest;
+    if (isWork) {
+      a -= 1;
+      digitalWrite(led, HIGH);
+      digitalWrite(air, HIGH);
+      digitalWrite(fan, HIGH);
+      Serial.println(a);
+      if (a <= 0) {
+        isWork = false;
+        a = rest;
+      }
+    } else {
+      b -= 1;
+      digitalWrite(led, LOW);
+      digitalWrite(air, LOW);
+      digitalWrite(fan, LOW);
+      Serial.println(b);
+      if (b <= 0) {
+        isWork = true;
+        b = work;
+      }
     }
   }
+  server.handleClient();
 }
+
+#endif
 
