15418c265dac0f63f7666d293dba5341c6f00418
Aibekin <aibekin_zh_007@mail.ru>
2024-09-27 19:55:04 +0500

    more details

--- src/scripts/loop.h
+++ src/scripts/loop.h
@@ -14,27 +14,27 @@ void loop_func()
 {
 	timer.attach(1, []()
 				 {
-    if (isWork) {
-      a -= 1;
-      digitalWrite(led, HIGH);
-      digitalWrite(air, HIGH);
-      digitalWrite(fan, HIGH);
-      Serial.println(a);
-      if (a <= 0) {
-        isWork = false;
-        a = rest;
-      }
-    } else {
-      b -= 1;
-      digitalWrite(led, LOW);
-      digitalWrite(air, LOW);
-      digitalWrite(fan, LOW);
-      Serial.println(b);
-      if (b <= 0) {
-        isWork = true;
-        b = work;
-      }
-    } });
+		if (isWork) {
+			b -= 1;
+			digitalWrite(led, HIGH);
+			digitalWrite(air, HIGH);
+			digitalWrite(fan, HIGH);
+			Serial.println(b);
+			if (b <= 0) {
+				isWork = false;
+				b = rest;
+			}
+		} else {
+			a -= 1;
+			digitalWrite(led, LOW);
+			digitalWrite(air, LOW);
+			digitalWrite(fan, LOW);
+			Serial.println(a);
+			if (a <= 0) {
+				isWork = true;
+				a = work;
+			}
+		} });
 }
 
 #endif
